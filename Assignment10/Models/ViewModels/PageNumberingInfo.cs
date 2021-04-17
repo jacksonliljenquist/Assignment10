8c025af758f7c860e8aa790903ee97dbeea23e02
Jackson Liljenquist <liljenqj@byu.edu>
2021-04-16 22:21:55 -0600

    Add project files.

---
+++ Assignment10/Models/ViewModels/PageNumberingInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Models.ViewModels
+{
+    public class PageNumberingInfo
+    {
+        public int NumItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalNumItems { get; set; }
+
+        //calc num of pages
+        public int NumPages => (int) (Math.Ceiling((decimal) TotalNumItems / NumItemsPerPage));
+
+
+
+    }
+}
